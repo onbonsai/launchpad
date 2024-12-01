9fff616cca8c53802fa4e0b6ee8c5b4b8a2533c5
Carlos Beltran <imthatcarlos@gmail.com>
2024-11-30 20:29:09 -0600

    fix: marketCap for trending

--- pages/api/clubs/get-trending-club.ts
+++ pages/api/clubs/get-trending-club.ts
@@ -1,21 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { decodeAbiParameters } from "viem";
+import { decodeAbiParameters, formatUnits } from "viem";
 import { groupBy } from "lodash/collection";
 
-import { getLatestTrades, getRegisteredClubById } from "@src/services/madfi/moneyClubs";
+import { getLatestTrades, getRegisteredClubById, DECIMALS } from "@src/services/madfi/moneyClubs";
 
 // super basic, the club that has the most buys in the last 100 trades
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const trades = await getLatestTrades();
     const grouped = groupBy(trades, "club.clubId");
-    console.log(grouped);
     const trendingClubId = Object.keys(grouped).reduce((a, b) => grouped[a].length > grouped[b].length ? a : b);
 
     const club = await getRegisteredClubById(trendingClubId);
     const [name, symbol, image] = decodeAbiParameters([
       { name: 'name', type: 'string' }, { name: 'symbol', type: 'string' }, { name: 'uri', type: 'string' }
     ], club.tokenInfo);
+    club.marketCap = BigInt(formatUnits(club.supply, DECIMALS)) * BigInt(club.currentPrice);
 
     club.token = {
       name,
