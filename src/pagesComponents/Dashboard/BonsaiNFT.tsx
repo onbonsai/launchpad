e354af2461740f0a21ab1136a5afcbb04decf90d
Steven Andrews <info@sandrews.io>
2024-12-17 21:49:33 -0500

    try to make nft images load

--- src/pagesComponents/Dashboard/BonsaiNFT.tsx
+++ src/pagesComponents/Dashboard/BonsaiNFT.tsx
@@ -8,37 +8,47 @@ interface BonsaiNFTProps {
 }
 
 function BonsaiNFT(props: BonsaiNFTProps) {
-  const { tree, index } = props;
-  const size = props.size || "78px";
-    console.log(tree.image);
-  const imageUrl = tree.image?.cachedUrl
-    ? tree.image.cachedUrl
-    : tree.raw?.metadata?.image
-      ? storjGatewayURL(tree.raw.metadata.image)
-      : "";
-
-  return (
-    <div
-      key={`tree-${index}`}
-      className="relative rounded-2xl overflow-hidden"
-      style={{
-        minWidth: size,
-        maxWidth: size,
-        height: size,
-      }}
-    >
-      <a href={tree.openseaUrl} target="_blank" rel="noreferrer">
-        <Image
-          src={imageUrl}
-          fill
-          className="object-cover"
-          alt="bonsai tree"
-          unoptimized={true}
-          sizes={size}
-        />
-      </a>
-    </div>
-  );
-}
+    const { tree, index } = props;
+    const size = props.size || "78px";
+    
+    const imageUrl = tree.image?.cachedUrl
+      ? tree.image.cachedUrl
+      : tree.raw?.metadata?.image
+        ? storjGatewayURL(tree.raw.metadata.image)
+        : "";
+  
+    const isSvg = imageUrl.toLowerCase().endsWith('.svg');
+    console.log('isSvg', isSvg);
+    return (
+      <div
+        key={`tree-${index}`}
+        className="relative rounded-2xl overflow-hidden"
+        style={{
+          minWidth: size,
+          maxWidth: size,
+          height: size,
+        }}
+      >
+        <a href={tree.openseaUrl} target="_blank" rel="noreferrer">
+          {isSvg ? (
+            <img
+              src={imageUrl}
+              className="object-cover w-full h-full"
+              alt="bonsai"
+            />
+          ) : (
+            <Image
+              src={imageUrl}
+              fill
+              className="object-cover"
+              alt="bonsai"
+              unoptimized={true}
+              sizes={size}
+            />
+          )}
+        </a>
+      </div>
+    );
+  }
 
 export default BonsaiNFT;
