075648e2fdb717851de9195e205427c3cacd047f
Nate Mueller <inathan.m@gmail.com>
2024-12-19 15:16:08 -0500

    stroke-width strokeWidth

--- pages/profile/PositiveIcon.tsx
+++ pages/profile/PositiveIcon.tsx
@@ -2,7 +2,7 @@ const PositiveIcon = () => {
     return (
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_292_879)">
-                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8Z" stroke="#0B9981" stroke-width="1.2"/>
+                <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" d="M1.5 8C1.5 4.41015 4.41015 1.5 8 1.5C11.5899 1.5 14.5 4.41015 14.5 8C14.5 11.5899 11.5899 14.5 8 14.5C4.41015 14.5 1.5 11.5899 1.5 8Z" stroke="#0B9981" strokeWidth="1.2"/>
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.38443 4.53909L8.00032 4.21899L7.61621 4.53909L4.61621 7.03909L5.38443 7.96095L7.40032 6.28104V11H8.60032V6.28104L10.6162 7.96095L11.3844 7.03909L8.38443 4.53909Z" fill="#0B9981"/>
             </g>
             <defs>
