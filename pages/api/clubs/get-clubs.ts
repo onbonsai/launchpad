5ac58c175848dbc6282828eb645178a4ea8dff24
Carlos Beltran <imthatcarlos@gmail.com>
2024-12-31 11:25:37 -0500

    include creator info in get clubs and get trending club

--- pages/api/clubs/get-clubs.ts
+++ pages/api/clubs/get-clubs.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { groupBy } from "lodash/collection";
 import { getClubs } from "@src/services/madfi/moneyClubs";
+import { getClientWithClubs } from "@src/services/mongo/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
@@ -7,10 +9,30 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     const data = await getClubs(page ? parseInt(page as string) : 0);
 
+    // enriched with featured, handle, etc
+    const clubIds = data?.clubs.map(({ clubId }) => parseInt(clubId));
+    const { collection } = await getClientWithClubs();
+    const _clubs = await collection.find(
+      { clubId: { $in: clubIds } },
+      { projection: { _id: 0 } }
+    ).toArray();
+    const groupedClubs = groupBy(_clubs || [], "clubId");
+    const clubs = data?.clubs.map((_club) => {
+      const club = groupedClubs[_club.clubId.toString()] ? groupedClubs[_club.clubId.toString()][0] : undefined;
+      if (!club) return _club;
+      return {
+        ..._club,
+        creatorStrategy: club.strategy,
+        creatorPubId: club.pubId,
+        creatorHandle: club.handle,
+        creatorProfileId: club.profileId
+      };
+    });
+
     // cache 60s
     res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate');
 
-    return res.status(200).json(data);
+    return res.status(200).json({ clubs, hasMore: data.hasMore });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ success: false });
--- pages/api/clubs/get-trending-club.ts
+++ pages/api/clubs/get-trending-club.ts
@@ -3,6 +3,7 @@ import { decodeAbiParameters, formatUnits } from "viem";
 import { groupBy } from "lodash/collection";
 
 import { getLatestTrades, getRegisteredClubById, DECIMALS } from "@src/services/madfi/moneyClubs";
+import { getClientWithClubs } from "@src/services/mongo/client";
 
 // super basic, the club that has the most buys in the last 100 trades
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,7 +12,25 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const grouped = groupBy(trades, "club.clubId");
     const trendingClubId = Object.keys(grouped).reduce((a, b) => grouped[a].length > grouped[b].length ? a : b);
 
-    const club = await getRegisteredClubById(trendingClubId);
+    const [club, enriched] = await Promise.all([
+      getRegisteredClubById(trendingClubId),
+      (async () => {
+        const { collection } = await getClientWithClubs();
+
+        const club = await collection.find(
+          { clubId: parseInt(trendingClubId) },
+          { projection: { _id: 0 } }
+        ).limit(1).next();
+
+        return {
+          creatorStrategy: club.strategy,
+          creatorPubId: club.pubId,
+          creatorHandle: club.handle,
+          creatorProfileId: club.profileId
+        };
+      })()
+    ]);
+
     const [name, symbol, image] = decodeAbiParameters([
       { name: 'name', type: 'string' }, { name: 'symbol', type: 'string' }, { name: 'uri', type: 'string' }
     ], club.tokenInfo);
@@ -26,7 +45,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     // cache 60s
     res.setHeader('Cache-Control', 'public, s-maxage=60, stale-while-revalidate');
 
-    return res.status(200).json(club);
+    return res.status(200).json({ ...club, ...enriched });
   } catch (e) {
     console.log(e);
     return res.status(500).json({ success: false });
