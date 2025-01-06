30638dbd3e6dae60a9549dc7d9ce1d8101d8cfd9
Carlos Beltran <imthatcarlos@gmail.com>
2025-01-06 17:32:25 -0500

    revert id override

--- pages/api/clubs/get-clubs.ts
+++ pages/api/clubs/get-clubs.ts
@@ -22,7 +22,6 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       if (!club) return _club;
       return {
         ..._club,
-        id: _club.clubId,
         creatorStrategy: club.strategy,
         creatorPubId: club.pubId,
         creatorHandle: club.handle,
