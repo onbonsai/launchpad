3a4c8ded830b531e879c1efd991eed01d2f19c11
Carlos Beltran <imthatcarlos@gmail.com>
2025-02-01 13:31:00 -0500

    add: api to set club pub id

---
+++ pages/api/clubs/set-publication-id.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { TransactionReceipt } from "viem";
+import { publicClient } from "@src/services/madfi/moneyClubs";
+import { getEventFromReceipt } from "@src/utils/viem";
+import { LAUNCHPAD_CONTRACT_ADDRESS } from "@src/services/madfi/utils";
+import BonsaiLaunchpadAbi from "@src/services/madfi/abi/BonsaiLaunchpad.json";
+import { getClientWithClubs } from "@src/services/mongo/client";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { txHash, pubId } = req.body;
+
+    const transactionReceipt: TransactionReceipt = await publicClient().waitForTransactionReceipt({ hash: txHash });
+    const registeredClubEvent = getEventFromReceipt({
+      contractAddress: LAUNCHPAD_CONTRACT_ADDRESS,
+      transactionReceipt,
+      abi: BonsaiLaunchpadAbi,
+      eventName: "RegisteredClub",
+    });
+    const { clubId } = registeredClubEvent.args;
+    if (!clubId) throw new Error("No registered club");
+
+    const { collection } = await getClientWithClubs();
+
+    // only update where pubId does not exist
+    await collection.updateOne(
+      { clubId: clubId.toNumber(), pubId: { $exists: false } },
+      { $set: { pubId } }
+    );
+
+    res.status(200);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ success: false });
+  }
+};
+
+export default handler;
