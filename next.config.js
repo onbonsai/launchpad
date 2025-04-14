6293cbc05e12636e2ebbbe074bee4d4890b717f3
Carlos Beltran <imthatcarlos@gmail.com>
2025-04-14 17:59:21 -0500

    add walletconnect verification

--- next.config.js
+++ next.config.js
@@ -25,6 +25,15 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/.well-known/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+      {
         source: '/opengraph-image.jpg',
         headers: [
           {
