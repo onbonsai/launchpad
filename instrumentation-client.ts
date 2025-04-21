a1640431a2a147b195706629bd4d9dafe2375917
Steven Andrews <info@sandrews.io>
2025-04-21 11:01:00 -0400

    Stop Sentry frpm running locally

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,10 +3,14 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
+if (!!process.env.NEXT_RUNTIME) {
 
-Sentry.init({
-  dsn: "https://04796f722151ea9b1fae0d561cff9c00@o4509125817073664.ingest.us.sentry.io/4509125819039744",
+  Sentry.init({
+    dsn: "https://04796f722151ea9b1fae0d561cff9c00@o4509125817073664.ingest.us.sentry.io/4509125819039744",
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+} else {
+  console.log("Sentry not initialized because NEXT_RUNTIME is not set");
+}
