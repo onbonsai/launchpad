13e9411588bc138928873b682f008b1175611ac7
Nate Mueller <inathan.m@gmail.com>
2025-04-24 20:35:38 -0500

    swap from gho if bonsai balance insufficient for collect

--- src/services/lens/collect.ts
+++ src/services/lens/collect.ts
@@ -2,11 +2,12 @@ import { postId, SessionClient } from "@lens-protocol/client";
 import { executePostAction } from "@lens-protocol/client/actions";
 import { handleOperationWith } from "@lens-protocol/client/viem";
 import { getChain, PROTOCOL_DEPLOYMENT } from "../madfi/utils";
-import { erc20Abi, parseEther } from "viem";
+import { erc20Abi, parseEther, formatEther, encodePacked } from "viem";
 import { toast } from "react-hot-toast";
-import { formatEther } from "viem";
 import { readContract } from "viem/actions";
-import { publicClient } from "../madfi/moneyClubs";
+import { publicClient, WGHO_ABI, WGHO_CONTRACT_ADDRESS } from "../madfi/moneyClubs";
+import { swapGhoForBonsai, calculatePath } from "./rewardSwap";
+import { quoteExactOutput } from "../uniswap/useQuote";
 
 export const collectPost = async (
   sessionClient: SessionClient,
@@ -63,40 +64,141 @@ export const checkCollectAmount = async (
       throw new Error("No user address found");
     }
   }
-  let toastId;
-  if (bonsaiBalance < collectAmountBn) {
-    const balance = await readContract(walletClient, {
+
+  // If lens account has enough BONSAI, we're done
+  if (bonsaiBalance >= collectAmountBn) {
+    return;
+  }
+
+  // Check EOA BONSAI balance
+  const eoaBonsaiBalance = await readContract(walletClient, {
+    address: PROTOCOL_DEPLOYMENT.lens.Bonsai,
+    abi: erc20Abi,
+    functionName: "balanceOf",
+    args: [userAddress],
+  });
+
+  const _publicClient = publicClient("lens");
+
+  // If EOA has enough BONSAI, transfer it to lens account
+  if (eoaBonsaiBalance + bonsaiBalance >= collectAmountBn) {
+    const amountToTransfer = collectAmountBn - bonsaiBalance;
+    const toastId = toast.loading(`Transferring ${formatEther(amountToTransfer)} $BONSAI to Lens account...`);
+
+    const transferResult = await walletClient.writeContract({
       address: PROTOCOL_DEPLOYMENT.lens.Bonsai,
       abi: erc20Abi,
-      functionName: "balanceOf",
-      args: [userAddress],
+      functionName: "transfer",
+      args: [lensAccount, amountToTransfer],
+      chain: getChain("lens"),
     });
 
-    if (bonsaiBalance + balance >= collectAmountBn) {
-      // Calculate the amount needed to transfer
-      const amountToTransfer = collectAmountBn - bonsaiBalance;
+    if (!transferResult) {
+      toast.error("Failed to transfer BONSAI tokens to lens account", { id: toastId });
+      throw new Error("Failed to transfer BONSAI tokens to lens account");
+    }
 
-      toastId = toast.loading(`Transferring ${formatEther(amountToTransfer)} $BONSAI to Lens account...`);
+    await _publicClient.waitForTransactionReceipt({ hash: transferResult });
+    toast.success("BONSAI tokens transferred to lens account", { id: toastId });
+    return;
+  }
 
-      // Transfer the needed amount from user wallet to lens account
-      const transferResult = await walletClient.writeContract({
-        address: PROTOCOL_DEPLOYMENT.lens.Bonsai,
-        abi: erc20Abi,
-        functionName: "transfer",
-        args: [lensAccount, amountToTransfer],
-        chain: getChain("lens"),
+  toast("Not enough BONSAI balance. Checking WGHO balance...", { duration: 10000 });
+
+  // If not enough BONSAI, check WGHO balance
+  const wghoBalance = await readContract(walletClient, {
+    address: WGHO_CONTRACT_ADDRESS,
+    abi: erc20Abi,
+    functionName: "balanceOf",
+    args: [userAddress],
+  });
+
+  // Get quote for how much WGHO needed to get required BONSAI
+  const requiredBonsai = collectAmountBn - (bonsaiBalance + eoaBonsaiBalance);
+  const quoteResult = await quoteExactOutput(
+    // path has to be in reverse order for quoteExactOutput
+    encodePacked(["address", "uint24", "address"], [PROTOCOL_DEPLOYMENT.lens.Bonsai, 3000, WGHO_CONTRACT_ADDRESS]),
+    (105n * requiredBonsai) / 100n,
+    userAddress,
+  );
+
+  // If enough WGHO, swap it to BONSAI
+  if (wghoBalance >= quoteResult.amount) {
+    const swapToastId = toast.loading("Swapping WGHO to BONSAI...");
+    try {
+      await swapGhoForBonsai(walletClient, (101n * requiredBonsai) / 100n, userAddress, quoteResult.amount);
+      toast.success("Swapped WGHO to BONSAI", { id: swapToastId });
+    } catch (error) {
+      toast.error("Failed to swap WGHO to BONSAI", { id: swapToastId });
+      throw error;
+    }
+  } else {
+    // Check GHO balance
+    const ghoBalance = await _publicClient.getBalance({
+      address: userAddress,
+    });
+
+    // Calculate how much more WGHO we need
+    const additionalWGHONeeded = quoteResult.amount - wghoBalance;
+
+    if (ghoBalance < additionalWGHONeeded) {
+      toast.error("Insufficient GHO balance");
+      throw new Error("Insufficient GHO balance");
+    }
+
+    // Wrap the required amount of GHO
+    const toastId = toast.loading("Wrapping GHO...");
+    try {
+      const wrapHash = await walletClient.writeContract({
+        address: WGHO_CONTRACT_ADDRESS,
+        abi: WGHO_ABI,
+        functionName: "deposit",
+        args: [],
+        value: additionalWGHONeeded,
       });
 
-      if (!transferResult) {
-        throw new Error("Failed to transfer BONSAI tokens to lens account");
-      }
+      await _publicClient.waitForTransactionReceipt({ hash: wrapHash });
+      toast.success("Wrapped GHO", { id: toastId });
+    } catch (error) {
+      toast.error("Failed to wrap GHO", { id: toastId });
+      throw error;
+    }
 
-      await publicClient("lens").waitForTransactionReceipt({ hash: transferResult });
-
-      toast.success("BONSAI tokens transferred to lens account", { id: toastId });
-    } else {
-      toast.error("Insufficient BONSAI balance", { id: toastId });
-      // TODO: attempt to swap GHO for BONSAI
+    // Now swap the WGHO to BONSAI
+    const swapToastId = toast.loading("Swapping WGHO to BONSAI...");
+    try {
+      await swapGhoForBonsai(walletClient, (101n * requiredBonsai) / 100n, userAddress, quoteResult.amount);
+      toast.success("Swapped WGHO to BONSAI", { id: swapToastId });
+    } catch (error) {
+      toast.error("Failed to swap WGHO to BONSAI", { id: swapToastId });
+      throw error;
     }
   }
+
+  // Finally, transfer the BONSAI to the lens account
+  const finalBonsaiBalance = await readContract(walletClient, {
+    address: PROTOCOL_DEPLOYMENT.lens.Bonsai,
+    abi: erc20Abi,
+    functionName: "balanceOf",
+    args: [userAddress],
+  });
+
+  const amountToTransfer = collectAmountBn - bonsaiBalance;
+  const transferToastId = toast.loading(`Transferring ${formatEther(amountToTransfer)} $BONSAI to Lens account...`);
+
+  const transferResult = await walletClient.writeContract({
+    address: PROTOCOL_DEPLOYMENT.lens.Bonsai,
+    abi: erc20Abi,
+    functionName: "transfer",
+    args: [lensAccount, amountToTransfer],
+    chain: getChain("lens"),
+  });
+
+  if (!transferResult) {
+    toast.error("Failed to transfer BONSAI tokens to lens account", { id: transferToastId });
+    throw new Error("Failed to transfer BONSAI tokens to lens account");
+  }
+
+  await _publicClient.waitForTransactionReceipt({ hash: transferResult });
+  toast.success("BONSAI tokens transferred to lens account", { id: transferToastId });
 };
--- src/services/lens/rewardSwap.ts
+++ src/services/lens/rewardSwap.ts
@@ -1,6 +1,8 @@
-import { encodePacked } from "viem";
-import { PROTOCOL_DEPLOYMENT } from "../madfi/utils";
-import { WGHO_CONTRACT_ADDRESS } from "../madfi/moneyClubs";
+import { encodeAbiParameters, encodePacked, zeroAddress } from "viem";
+import { ACTION_HUB_ADDRESS, LENS_GLOBAL_FEED, PROTOCOL_DEPLOYMENT } from "../madfi/utils";
+import { publicClient, SWAP_TO_BONSAI_POST_ID, WGHO_CONTRACT_ADDRESS } from "../madfi/moneyClubs";
+import ActionHubAbi from "@src/services/madfi/abi/ActionHub.json";
+import { quoteExactOutput } from "../uniswap/useQuote";
 
 // Lens reward swap parameter constants
 export const PARAM__PATH = "0xc933ed7045acf6fe8798b8a8ab584b953eb4e2ea05683ebbe5eb3617c481b1f2" as const;
@@ -20,13 +22,48 @@ export const calculatePath = (tokenAddress: `0x${string}` | string, fromAddress?
     );
   }
   if (fromAddress === PROTOCOL_DEPLOYMENT.lens.Bonsai) {
-    return encodePacked(
-      ["address", "uint24", "address"],
-      [PROTOCOL_DEPLOYMENT.lens.Bonsai, 10000, tokenAddress],
-    );
+    return encodePacked(["address", "uint24", "address"], [PROTOCOL_DEPLOYMENT.lens.Bonsai, 10000, tokenAddress]);
   }
   return encodePacked(
     ["address", "uint24", "address", "uint24", "address"],
     [WGHO_CONTRACT_ADDRESS, 3000, PROTOCOL_DEPLOYMENT.lens.Bonsai, 10000, tokenAddress],
   );
+};
+
+/**
+ * Swap GHO for BONSAI
+ * @param walletClient
+ * @param amountOut Amount of BONSAI desired
+ */
+export const swapGhoForBonsai = async (walletClient: any, amountOut: bigint, account: string, amountIn?: bigint) => {
+  const quoteResult = amountIn
+    ? { amount: amountIn }
+    : // If no amountIn, get quote for amountOut, reverse path
+      await quoteExactOutput(
+        encodePacked(["address", "uint24", "address"], [PROTOCOL_DEPLOYMENT.lens.Bonsai, 3000, WGHO_CONTRACT_ADDRESS]),
+        amountOut,
+        account,
+      );
+  const hash = await walletClient.writeContract({
+    address: ACTION_HUB_ADDRESS,
+    abi: ActionHubAbi,
+    functionName: "executePostAction",
+    args: [
+      PROTOCOL_DEPLOYMENT.lens.RewardSwap,
+      LENS_GLOBAL_FEED,
+      SWAP_TO_BONSAI_POST_ID,
+      [
+        {
+          key: PARAM__PATH,
+          value: encodeAbiParameters([{ type: "bytes" }], [calculatePath(PROTOCOL_DEPLOYMENT.lens.Bonsai)]),
+        },
+        { key: PARAM__AMOUNT_IN, value: encodeAbiParameters([{ type: "uint256" }], [quoteResult.amount]) },
+        { key: PARAM__AMOUNT_OUT_MINIMUM, value: encodeAbiParameters([{ type: "uint256" }], [amountOut]) },
+        { key: PARAM__CLIENT_ADDRESS, value: encodeAbiParameters([{ type: "address" }], [zeroAddress]) },
+        { key: PARAM__REFERRALS, value: encodeAbiParameters([{ type: "address[]" }], [[]]) },
+      ],
+    ],
+  });
+
+  await publicClient("lens").waitForTransactionReceipt({ hash });
 };
