97ff74de02aa4dc81137047bdd56bf72364d7f6b
Carlos Beltran <imthatcarlos@gmail.com>
2025-04-25 12:02:28 -0500

    add lens rpc to thirdweb

--- src/services/thirdweb/client.ts
+++ src/services/thirdweb/client.ts
@@ -1,7 +1,7 @@
 import { createThirdwebClient, defineChain } from "thirdweb";
 import { viemAdapter } from "thirdweb/adapters/viem";
 
-export const lensChain = defineChain({ id: 232 });
+export const lensChain = defineChain({ id: 232, rpc: process.env.NEXT_PUBLIC_LENS_RPC });
 
 export const thirdwebWallet = async (walletClient) => {
   return await viemAdapter.wallet.fromViem({
