64d6c83a061822507c8b685c3f6f1c90eeb9554a
Nate Mueller <inathan.m@gmail.com>
2025-05-18 13:49:10 -0500

    fix bad Image src

--- src/components/Creators/CreatorButton.tsx
+++ src/components/Creators/CreatorButton.tsx
@@ -1,32 +1,30 @@
-import { shortAddress } from '@src/utils/utils';
-import clsx from 'clsx';
-import React from 'react'
-import { isAddress } from 'viem';
-import Image from 'next/image';
+import { shortAddress } from "@src/utils/utils";
+import clsx from "clsx";
+import React from "react";
+import { isAddress } from "viem";
 
 interface CreatorButtonProps {
-    image?: string;
-    text: string;
-    onClick?: () => void;
+  image?: string;
+  text: string;
+  onClick?: () => void;
 }
 
 const CreatorButton = (props: CreatorButtonProps) => {
-    const { image, text, onClick } = props
-    const handle = isAddress(text) ? shortAddress(text) : text;
-    return (
-        <div onClick={onClick} className={clsx("flex flex-row gap-[6px] bg-card rounded-lg", onClick ? "hover" : '', image ? 'p-[2px] pr-[8px]' : 'px-2 py-1')}>
-            {image && <Image
-                src={image}
-                alt="user"
-                className="w-5 h-5 rounded-md"
-                width={20}
-                height={20}
-            />}
-            <p className={clsx("text-sm font-medium tracking-[-0.02em]", image ? '' : 'leading-4')}>
-                {handle}
-            </p>
-        </div>
-    )
-}
+  const { image, text, onClick } = props;
+  const handle = isAddress(text) ? shortAddress(text) : text;
+  return (
+    <div
+      onClick={onClick}
+      className={clsx(
+        "flex flex-row gap-[6px] bg-card rounded-lg",
+        onClick ? "hover" : "",
+        image ? "p-[2px] pr-[8px]" : "px-2 py-1",
+      )}
+    >
+      {image && <img src={image} alt="user" className="w-5 h-5 rounded-md" />}
+      <p className={clsx("text-sm font-medium tracking-[-0.02em]", image ? "" : "leading-4")}>{handle}</p>
+    </div>
+  );
+};
 
-export default CreatorButton
+export default CreatorButton;
