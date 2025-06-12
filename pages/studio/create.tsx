14e6299dfdaeee0770b0bd76bb96fddaad24b6a4
Carlos Beltran <imthatcarlos@gmail.com>
2025-06-12 11:43:28 -0500

    capture exception

--- pages/studio/create.tsx
+++ pages/studio/create.tsx
@@ -233,7 +233,7 @@ const StudioCreatePage: NextPage = () => {
           } catch (error) {
             console.log(error);
             Sentry.captureException(error);
-            toast.error(`Please switch to ${finalTokenData.selectedNetwork}`);
+            toast.error(`Please switch to ${finalTokenData.selectedNetwork}`, { id: toastId });
             setIsCreating(false);
             return;
           }
@@ -373,11 +373,12 @@ const StudioCreatePage: NextPage = () => {
       try {
         await switchChain(walletClient, { id: LENS_CHAIN_ID });
         // HACK: require lens chain for the whole thing
-          setIsCreating(false);
-          return;
+        setIsCreating(false);
+        return;
       } catch (error) {
         console.log(error);
-        toast.error("Please switch networks to create your Lens post");
+        Sentry.captureException(error);
+        toast.error("Please switch networks to create your Lens post", { id: toastId });
         setIsCreating(false);
         return;
       }
@@ -393,7 +394,8 @@ const StudioCreatePage: NextPage = () => {
       if (creds.isOk()) {
         idToken = creds.value?.idToken;
       } else {
-        toast.error("Failed to get credentials");
+        Sentry.captureException("Failed to get credentials");
+        toast.error("Failed to get credentials", { id: toastId });
         return;
       }
     }
