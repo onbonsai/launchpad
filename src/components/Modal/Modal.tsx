97d68cacdb7bf2f701e38982d7e9d1fdb78c64d1
Nate Mueller <inathan.m@gmail.com>
2025-06-30 13:50:56 -0400

    clean up mobile modal drawer animations

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -49,16 +49,16 @@ export const Modal: FC<ModalProps> = ({ open, onClose, setOpen, children, panelC
           <div className={`flex min-h-[100dvh] ${isMobile ? 'items-end' : 'items-center'} justify-center p-0 text-center md:p-4`}>
             <Transition.Child
               as={Fragment}
-              enter="transform transition-all ease-in-out duration-[500ms]"
-              enterFrom={isMobile ? "opacity-0 translate-y-full" : "opacity-0 translate-y-4 scale-95"}
+              enter={`transform transition-all ease-in-out duration-150`}
+              enterFrom={isMobile ? "opacity-100 translate-y-1/2" : "opacity-0 translate-y-4 scale-95"}
               enterTo={isMobile ? "opacity-100 translate-y-0" : "opacity-100 translate-y-0 scale-100"}
-              leave="transform transition-all ease-in duration-[300ms]"
+              leave={`transform transition-all ease-in duration-200`}
               leaveFrom={isMobile ? "opacity-100 translate-y-0" : "opacity-100 translate-y-0 scale-100"}
-              leaveTo={isMobile ? "opacity-0 translate-y-full" : "opacity-0 translate-y-4 scale-95"}
+              leaveTo={isMobile ? "opacity-100 translate-y-full" : "opacity-0 translate-y-4 scale-95"}
             >
               <Dialog.Panel
                 className={`backdrop-blur-[40px] bg-card relative text-left shadow-xl w-full max-h-[90vh] md:max-h-none md:my-8 md:max-w-lg md:min-w-[512px] overflow-auto h-auto md:p-4
-                  ${isMobile ? 'rounded-t-2xl' : 'rounded-lg'} ${panelClassnames}`
+                  ${isMobile ? 'rounded-t-2xl pb-6' : 'rounded-lg'} ${panelClassnames}`
                 }
               >
                 <div className="absolute top-0 right-0 mt-4 mr-4 sm:block z-50">
@@ -70,10 +70,6 @@ export const Modal: FC<ModalProps> = ({ open, onClose, setOpen, children, panelC
                     <span className="sr-only">Close</span>
                     <XIcon className="h-4 w-4" aria-hidden="true" />
                   </button>
-                </div>
-
-                {/* Mobile drag indicator */}
-                <div className="flex justify-center pt-3 pb-2 md:hidden">
                 </div>
 
                 {children}
